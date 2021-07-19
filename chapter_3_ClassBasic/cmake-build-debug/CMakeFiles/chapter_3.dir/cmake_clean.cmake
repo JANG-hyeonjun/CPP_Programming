320c58c0d576d6501f446be342fe5daf25d998fb
Jang-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-07-20 00:22:50 +0900

    commit auto 2021-07-19.md push

---
+++ chapter_3_ClassBasic/cmake-build-debug/CMakeFiles/chapter_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter_3.dir/main.cpp.o"
+  "chapter_3"
+  "chapter_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
