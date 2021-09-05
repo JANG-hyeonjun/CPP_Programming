d00c4244a31a41dc9eb7da7a17e01a692d215356
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-09-05 23:33:29 +0900

    push

---
+++ chapter04-2(Encapsulation)/Encaps1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class SinivelCap
+{
+public:
+    void Take() const {cout << "콧물이 싹 ~ 납니다" << endl;}
+};
+
+class SneezeCap
+{
+public:
+    void Take() const {cout << "재채기가 멎습니다" << endl;}
+};
+
+class SnuffleCap
+{
+public:
+    void Take() const {cout << "코가 뻥 뚫립니다." << endl;}
+};
+
+class ColdPatioent
+{
+public:
+    void TakeSinivelCap(const SinivelCap &cap) const {cap.Take();}
+    void TakeSneezeCap(const SneezeCap &cap) const {cap.Take();}
+    void TakeSnuffleCap(const SnuffleCap &cap) const {cap.Take();}
+};
+
+int main() {
+
+
+    SinivelCap scap;
+    SneezeCap zcap;
+    SnuffleCap ncap;
+
+    ColdPatioent sufferer;
+    sufferer.TakeSinivelCap(scap);
+    sufferer.TakeSneezeCap(zcap);
+    sufferer.TakeSnuffleCap(ncap);
+
+
+    return 0;
+}
---
+++ chapter04-2(Encapsulation)/cmake-build-debug/CMakeFiles/chapter04_2_Encapsulation_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter04_2_Encapsulation_.dir/Encaps1.cpp.o"
+  "chapter04_2_Encapsulation_"
+  "chapter04_2_Encapsulation_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter04_2_Encapsulation_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
