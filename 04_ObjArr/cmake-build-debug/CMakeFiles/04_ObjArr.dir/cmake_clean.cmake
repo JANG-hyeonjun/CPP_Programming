b31c0500b8f8cff3f5f21e5f7fb2fa7086d32bdb
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-10-28 23:33:49 +0900

    study objArr

---
+++ 04_ObjArr/cmake-build-debug/CMakeFiles/04_ObjArr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "04_ObjArr"
+  "04_ObjArr.pdb"
+  "CMakeFiles/04_ObjArr.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/04_ObjArr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
