2b34c5f039f4937f81dacc2831d894d1c5c16bb8
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-10-29 00:03:04 +0900

    commit auto 2021-10-28.md push

---
+++ PointerThis/cmake-build-debug/CMakeFiles/PointerThis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PointerThis.
+# This may be replaced when dependencies are built.
---
+++ objPtrArr/cmake-build-debug/CMakeFiles/objPtrArr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/objPtrArr.dir/main.cpp.o"
+  "objPtrArr"
+  "objPtrArr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/objPtrArr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
