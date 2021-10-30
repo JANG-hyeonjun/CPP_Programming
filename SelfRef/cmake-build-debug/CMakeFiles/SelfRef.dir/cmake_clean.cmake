3567d6e429981cdb6a308cb55d9b6435eb6ccf03
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-10-31 00:50:30 +0900

    study

---
+++ SelfRef/cmake-build-debug/CMakeFiles/SelfRef.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SelfRef.dir/main.cpp.o"
+  "SelfRef"
+  "SelfRef.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SelfRef.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
