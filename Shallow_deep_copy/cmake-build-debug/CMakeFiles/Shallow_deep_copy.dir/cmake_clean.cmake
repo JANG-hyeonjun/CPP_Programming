c1ec8217fdfadeb9147a79c9a7deb07ee98c3f39
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-11-21 23:57:03 +0900

    commit auto 2021-11-21.md push

---
+++ Shallow_deep_copy/cmake-build-debug/CMakeFiles/Shallow_deep_copy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shallow_deep_copy.dir/main.cpp.o"
+  "Shallow_deep_copy"
+  "Shallow_deep_copy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shallow_deep_copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ problem05_1/cmake-build-debug/CMakeFiles/problem05_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem05_1.dir/main.cpp.o"
+  "problem05_1"
+  "problem05_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem05_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
