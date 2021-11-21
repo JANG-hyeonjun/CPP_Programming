0e27bdc985b3f991cb1bade4771fb2bbbdb5de7d
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2021-11-21 14:50:05 +0900

    study

---
+++ ClassInit/cmake-build-debug/CMakeFiles/ClassInit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassInit.dir/main.cpp.o"
+  "ClassInit"
+  "ClassInit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassInit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
