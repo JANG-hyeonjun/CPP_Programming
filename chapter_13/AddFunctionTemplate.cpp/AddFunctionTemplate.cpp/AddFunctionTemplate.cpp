58b66277c7ba3b9325acda5df7ecc3d369d206e9
JANG-hyeonjun <cjstkek0907@hufs.ac.kr>
2022-02-07 23:23:25 +0900

    study chapter 13

---
+++ chapter_13/AddFunctionTemplate.cpp/AddFunctionTemplate.cpp/AddFunctionTemplate.cpp
@@ -0,0 +1,23 @@
+﻿// AddFunctionTemplate.cpp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+template <typename T>
+
+T Add(T num1, T num2)
+{
+	return num1 + num2; 
+}
+
+
+int main()
+{
+	std::cout << Add<int>(15, 20) << std::endl;
+	std::cout << Add<double>(2.9, 3.7)<< std::endl;
+	std::cout << Add<int>(3.2, 3.2) << std::endl;
+	std::cout << Add<double>(3.14, 2.75) << std::endl;
+	
+	return EXIT_SUCCESS;
+}
+
---
+++ chapter_13/TwoTypeAddFunction/TwoTypeAddFunction/TwoTypeAddFunction.cpp
@@ -0,0 +1,35 @@
+﻿// TwoTypeAddFunction.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+template <typename T>
+
+T Add(T num1, T num2)
+{
+	std::cout << "T Add(T num1, T num2)" << std::endl;
+	return num1 + num2;
+}
+
+int Add(int num1, int num2)
+{
+	std::cout << "Add(int num1, int num2)" << std::endl;
+	return num1 + num2;
+}
+
+double Add(double num1, double num2)
+{
+	std::cout << "Add(double num1, double num2)" << std::endl;
+	return num1 + num2;
+}
+
+int main()
+{
+	std::cout << Add(5, 7) << std::endl;
+	std::cout << Add(3.7, 7.5) << std::endl;
+	std::cout << Add<int>(5, 7) << std::endl;
+	std::cout << Add<double>(3.7, 5.7) << std::endl;
+
+	return 0;
+}
+
